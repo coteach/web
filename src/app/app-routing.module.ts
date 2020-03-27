f779c107a4acd8a9514a0523b3dd2d89ec67d796
Kahou <creator.kahou@gmail.com>
2020-03-27 18:29:28 +0800

    style: create scaffold component
    Set the primary color to white.
    Set the accent color to pink.
    Create scaffold component with a mater layout.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ScaffoldComponent } from './scaffold/scaffold.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: ScaffoldComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/scaffold/scaffold.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scaffold',
+  templateUrl: './scaffold.component.html',
+  styleUrls: ['./scaffold.component.scss']
+})
+export class ScaffoldComponent { }
