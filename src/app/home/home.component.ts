476c2282d04c10ebd1de53623b46674ab5acd4b7
Kahou <kahou@ducker.app>
2020-09-21 08:07:17 +0800

    style: support mobile device

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,7 @@ const TagNames: string[] = ["國小", "國中", "高中", "1年級", "2年級", 
   "9年級", "10年級", "11年級", "12年級", "國語文", "英語文", "本土語文", "數學", "自然科學", "生物", "理化", "物理", "化學", "地科",
   "生活課程", "社會", "歷史", "地理", "公民", "資訊科技", "生活科技", "藝術", "全民國防教育", "班級經營",];
 
-const Colors: string[] = ["red", "orange", "green", "blue", "purple", "black"];
+const Colors: string[] = ["red", "orange", "green", "purple", "black"];
 
 @Component({
   selector: 'app-home',
