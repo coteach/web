d8cf3bccb60c4151410463788f04c841eeded892
Kahou <creator.kahou@outlook.com>
2020-11-21 02:41:40 +0800

    build: rename `planhub` to `coteach`

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'planhub'`, () => {
+  it(`should have as title 'coteach'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('planhub');
+    expect(app.title).toEqual('coteach');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('planhub app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('coteach app is running!');
   });
 });
