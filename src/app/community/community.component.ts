10d95e2b72b708856518c21405e09ef3fc99d874
Ivy <ivychang167@gmail.com>
2020-12-03 19:44:38 +0800

    create: community page

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -9,6 +9,14 @@ export class CommunityComponent implements OnInit {
 
   constructor() { }
 
+  links = [
+    {title: '教育創新與另類教育討論', url: 'https://www.facebook.com/groups/educationinnovation/' },
+    {title:'瑩光教育讀書會',url:'https://www.facebook.com/groups/551317922040666/'},
+    {title:'均一教育後援會',url:'https://www.facebook.com/groups/junyiacademy/'},
+    {title:'公民教學社群',url:'https://www.facebook.com/groups/264913330343248/'},
+    {title: '學思達教學社群', url: 'https://www.facebook.com/groups/780188175346334/' },
+  ]
+
   ngOnInit(): void {
   }
 
